30a910efae114642354cc39c5ce4f35525405d6e
AlanOliveira1998 <alan.pires.oliveira@gmail.com>
2021-02-25 15:59:13 -0300

    Acertei o 22

--- Exercicios Python/ex0022.py
+++ Exercicios Python/ex0022.py
@@ -1,6 +1,5 @@
 #Faça um programa que abra e reproduza um arquivo de audio MP3
-import pygame
-pygame.init()
-pygame.mixer.music.load()
-pygame.mixer.music.play()
-pygame.event.wait()
+#pygame.init()
+#pygame.mixer.music.load()
+#pygame.mixer.music.play()
+#pygame.event.wait()
