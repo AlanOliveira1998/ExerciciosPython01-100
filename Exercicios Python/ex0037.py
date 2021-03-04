c901471440bbbfb347d525c2135fadf51f140dce
AlanOliveira1998 <alan.pires.oliveira@gmail.com>
2021-03-04 17:24:28 -0300

    continuação do curso 04/03/2021

---
+++ Exercicios Python/ex0037.py
@@ -0,0 +1,2 @@
+# Escreva um prgrama para aprovar o emprestimo bancário para compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o emprestimo será negado.
---
+++ Exercicios Python/ex0038.py
@@ -0,0 +1,1 @@
+# Escreva um programa que leia um numero inteiro qualquer e peça para o usuario escolher qual será a base de conversão: - 1 para binária; - 2 para octal; - 3 para hexadecimal; Bases numéricas
