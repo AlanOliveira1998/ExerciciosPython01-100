f9ece25f3336c858899173337753804505f7825f
AlanOliveira1998 <alan.pires.oliveira@gmail.com>
2021-03-04 14:35:50 -0300

    Finalizei o mundo 1 de python

---
+++ Exercicios Python/Aula 11 - Cores no Terminal.py
@@ -0,0 +1,6 @@
+print('\033[7;33mOla mundo\033[m')
+a = 3
+b = 5
+print(f'Os valores são \033[32m{a} e \033[31m{b}')
+nome = 'Guanabara'
+print('Olá, prazer em te conhecer, {}{}{}'.format('\033[4;34m', nome, '\033[m' ))
